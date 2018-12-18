2b2cb29996a2887de5308d88e6dc1087d9c36f52
arafatrakib1 <msarakib3@gmail.com>
2018-12-18 20:24:05 +0600

    version 0.1

--- report.py
+++ report.py
@@ -4,19 +4,30 @@ import sys
 from pathlib import Path
 from shutil import copy2
 
+
 if len(sys.argv) < 3:
     print('Please provide two arguments:\n first one is text file location\n second one is target directory location')
     sys.exit()
 
 EXCLUDE_DIRECTORY_COUNT = 4
-target_dir = sys.argv[2]
-temp_dir = target_dir.rstrip('/') + '/'
+file = sys.argv[1]
+target_dir = Path(sys.argv[2])
 
-with open(sys.argv[1]) as f:
+if not os.path.exists(file):
+    sys.exit('Invalid file')
+
+total = 0
+with open(file, 'r') as f:
     for line in f:
-        print("Copying " + line)
-        path = Path(line)
-        dir = temp_dir + '/'.join(path.parts[EXCLUDE_DIRECTORY_COUNT:-1])
+        if len(line) <= 1:
+            print('continue')
+            continue
+        path = line.strip('\n')
+        print("Copying " + path)
+        path = Path(path)
+        dir = target_dir.joinpath(*path.parts[EXCLUDE_DIRECTORY_COUNT:-1])
         if not os.path.exists(dir):
             os.makedirs(dir)
-        copy2(line.strip('\n'), dir)
+        copy2(path, dir)
+        total += 1
+print('copied ' + str(total))
